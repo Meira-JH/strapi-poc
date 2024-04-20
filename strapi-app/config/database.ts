ce2484148f7bbd226ac31a1680a206740af22d15
Meira-JH <jhalvesmeira@gmail.com>
2024-04-19 21:14:57 -0300

    configuration of docker, dockerfile and gke files

--- strapi-app/config/database.ts
+++ strapi-app/config/database.ts
@@ -59,7 +59,8 @@ export default ({ env }) => {
         host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi_user"),
+        schema: env("DATABASE_SCHEMA", "public"),
+        user: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi_password"),
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
@@ -72,7 +73,6 @@ export default ({ env }) => {
             true
           ),
         },
-        schema: env("DATABASE_SCHEMA", "public"),
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
